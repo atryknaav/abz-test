2e2231b72eaf7baa018b926322054237df7ad774
atryknaav <aetryknaavzaaroyt@gmail.com>
2024-08-25 16:24:08 +0300

    Initialized

---
+++ resources/js/Pages/Welcome.tsx
@@ -0,0 +1,27 @@
+import { Link, Head } from '@inertiajs/react';
+import { PageProps } from '@/types';
+import Authenticated from '@/Layouts/AuthenticatedLayout';
+import Guest from '@/Layouts/GuestLayout';
+
+export default function Welcome({ auth, laravelVersion, phpVersion }: PageProps<{ laravelVersion: string, phpVersion: string }>) {
+    const handleImageError = () => {
+        document.getElementById('screenshot-container')?.classList.add('!hidden');
+        document.getElementById('docs-card')?.classList.add('!row-span-1');
+        document.getElementById('docs-card-content')?.classList.add('!flex-row');
+        document.getElementById('background')?.classList.add('!hidden');
+    };
+
+    return (
+        <Guest 
+        
+    >
+        <Head title='Hi'/>
+
+        <div>
+            Hi
+        </div>
+
+
+        </Guest>
+    );
+}
