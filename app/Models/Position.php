036a3c0b3df5ca60f37f2ef8ed4f994b5311a163
atryknaav <aetryknaavzaaroyt@gmail.com>
2024-08-27 07:53:08 +0300

    Repository CC

--- app/Models/Position.php
+++ app/Models/Position.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Position extends Model
 {
+    protected $fillable = [
+        'name'
+    ];
     use HasFactory;
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Position;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,7 +14,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $positions = [
+            'Lawyer',
+            'Content Manager',
+            'Security',
+            'Designer',
+        ];
+
         User::factory(45)->create();
+        foreach($positions as $position){
+            Position::create([
+                'name'=>$position
+            ]);
+        }
 
         // User::factory()->create([
         //     'name' => 'Test User',
